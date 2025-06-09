5ca8d4153124ebcdd6643e7ff874c913edf587d1
aimanyusufw <aimanyusufdev@gmail.com>
2025-06-09 20:25:32 +0700

    feat : get categories

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,10 @@
 <?php
 
 use App\Http\Middleware\ValidateApiToken;
+use App\Http\Resources\CategoryResource;
 use App\Http\Resources\PostCardResource;
 use App\Http\Resources\PostResource;
+use App\Models\Category;
 use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -25,5 +27,20 @@ Route::middleware(ValidateApiToken::class)->group(function () {
         $data = $post->load("author", "category", "tags");
         $posts = $post->relatedArticles();
         return responseModel(200, "Get post successfully", ["post" => new PostResource($data), "related_posts" => PostCardResource::collection($posts)]);
+    });
+
+    Route::get('categories', function (Request $request) {
+        $countPerPage = $request->input('per_page', 5);
+        $getPosts = filter_var($request->input('with_posts'), FILTER_VALIDATE_BOOLEAN);
+
+        $query = Category::query();
+
+        if ($getPosts) {
+            $query->with('posts');
+        }
+
+        $categories = $query->paginate($countPerPage);
+
+        return responseModel(200, 'Get categories successfully', CategoryResource::collection($categories));
     });
 });
